c688191d30d719e55ce329307ddbc73c7a4a020d
Foshati <amirrezafoshati@gmail.com>
2024-10-18 23:07:06 +0330

    add react router dom and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Routes>
+      <Route path="/" element={<h2>home page</h2>} />
+      <Route path="/add" element={<h2>add post page</h2>} />
+      <Route path="/post/:id">
+        <Route index element={<h2> view post</h2>} />
+        <Route path="edit" element={<h2>edit post</h2>} />
+      </Route>
+    </Routes>
+  );
 }
