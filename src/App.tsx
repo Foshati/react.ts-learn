868ebeac0238ecb2ddf59fc79f39fe61cb73640e
Foshati <amirrezafoshati@gmail.com>
2024-10-19 23:31:12 +0330

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AddPost from "./components/AddPost";
+import Post from "./components/Post";
+import EditPost from "./components/EditPost";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<h2>home page</h2>} />
-      <Route path="/add" element={<h2>add post page</h2>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/add" element={<AddPost />} />
       <Route path="/post/:id">
-        <Route index element={<h2> view post</h2>} />
-        <Route path="edit" element={<h2>edit post</h2>} />
+        <Route index element={<Post />} />
+        <Route path="edit" element={<EditPost />} />
       </Route>
     </Routes>
   );
