1b38ab73417f3a6c5c28717da9bd3eee7f954b50
Foshati <amirrezafoshati@gmail.com>
2024-10-20 23:28:18 +0330

    create Post components

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Post() {
   return (
-    <div>Post</div>
-  )
+    <div className="m-4">
+      <form className="flex justify-center items-center gap-4">
+        <input className="input bg-black"  type="text" />
+        <textarea className=" textarea bg-black " name="" id=""></textarea>
+        <button className="btn" type="submit">Publish the post</button>
+        <Link to="..">
+          <button className="btn">cancel</button>
+        </Link>
+      </form>
+    </div>
+  );
 }
