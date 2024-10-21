b9ebf0a565ae6cd96f2b1c05dd72b3c7fff1f8c6
Foshati <amirrezafoshati@gmail.com>
2024-10-21 22:58:45 +0330

    add useRef

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,17 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
 
 export default function Post() {
+  const title = useRef<HTMLInputElement>(null)
+  const content = useRef<HTMLTextAreaElement>(null)
+  const tag= useRef(null)
   return (
     <div className="m-4">
       <form className="flex justify-center items-center gap-4">
-        <input className="input bg-black"  type="text" />
-        <textarea className=" textarea bg-black " name="" id=""></textarea>
+        <input ref={title} className="input bg-black"  type="text" />
+        <textarea ref={content} className=" textarea bg-black " name="" id=""></textarea>
+        <ReactSelect ref={tag} className="bg-black"  isMulti placeholder="choose"/>
         <button className="btn" type="submit">Publish the post</button>
         <Link to="..">
           <button className="btn">cancel</button>
