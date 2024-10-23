97bd9d7c64ca60f0623a2d543117123ce28cb297
Foshati <amirrezafoshati@gmail.com>
2024-10-23 23:45:28 +0330

    creat AddPost component

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,5 +1,24 @@
-export default function AddPost() {
+import PostForm from './PostForm'
+
+import { PostData, Tag } from '../App'
+
+type AddPostProps = {
+  onSubmit: (data: PostData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+function AddPost({ onSubmit, onAddTag, availableTags }: AddPostProps) {
   return (
-    <div>AddPost</div>
+    <>
+      <h2 className='mb-4'>افزودن پست</h2>
+      <PostForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
   )
 }
+
+export default AddPost
