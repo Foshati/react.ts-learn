99fc54037fb94af8372414ab6849a9fb579f65ce
Foshati <amirrezafoshati@gmail.com>
2024-10-27 23:46:35 +0330

    fix edit post

--- src/components/EditPost.tsx
+++ src/components/EditPost.tsx
@@ -1,5 +1,31 @@
-export default function EditPost() {
+import PostForm from './PostForm'
+
+import { PostData, Tag } from '../App'
+
+import { usePost } from './PostLayout'
+
+type EditPostProps = {
+  onSubmit: (id: string, data: PostData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+function EditPost({ onSubmit, onAddTag, availableTags }: EditPostProps) {
+  const post = usePost()
+
   return (
-    <div>EditPost</div>
+    <>
+      <h2 className='mb-4'>ویرایش پست</h2>
+      <PostForm
+        title={post.title}
+        markdown={post.markdown}
+        tags={post.tags}
+        onSubmit={(data) => onSubmit(post.id, data)}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
   )
 }
+
+export default EditPost
